acfc5bdb8e6b7a0767f80ff1571a4f3ee3ecbe27
xuan.liu <xuan.liu@1012wellness.com>
2021-08-25 16:33:26 +0800

    first init

---
+++ build/utils.ts
@@ -0,0 +1,46 @@
+// import fs from 'fs';
+// import path from 'path';
+// import dotenv from 'dotenv';
+
+export function isDevFn(mode: string): boolean {
+  return mode === 'development';
+}
+
+export function isProdFn(mode: string): boolean {
+  return mode === 'production';
+}
+
+/**
+ * Whether to generate package preview
+ */
+export function isReportMode(): boolean {
+  return process.env.REPORT === 'true';
+}
+
+// Read all environment variable configuration files to process.env
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+  const ret: any = {};
+
+  for (const envName of Object.keys(envConf)) {
+    let realName = envConf[envName].replace(/\\n/g, '\n');
+    realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+
+    if (envName === 'VITE_PORT') {
+      realName = Number(realName);
+    }
+    if (envName === 'VITE_PROXY') {
+      try {
+        realName = JSON.parse(realName);
+      } catch (error) {
+        realName = '';
+      }
+    }
+    ret[envName] = realName;
+    if (typeof realName === 'string') {
+      process.env[envName] = realName;
+    } else if (typeof realName === 'object') {
+      process.env[envName] = JSON.stringify(realName);
+    }
+  }
+  return ret;
+}
---
+++ build/vite/plugin/index.ts
@@ -0,0 +1,48 @@
+import type { Plugin } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import legacy from '@vitejs/plugin-legacy';
+
+import purgeIcons from 'vite-plugin-purge-icons';
+import { configHmrPlugin } from './hmr';
+import { configHtmlPlugin } from './html';
+import { configSvgIconsPlugin } from './svgSprite';
+import { configMockPlugin } from './mock';
+import { configStyleImportPlugin } from './styleImport';
+import { configVisualizerConfig } from './visualizer';
+import { configImageminPlugin } from './imagemin';
+
+export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
+  const { VITE_USE_IMAGEMIN, VITE_USE_MOCK, VITE_LEGACY } = viteEnv;
+
+  const vitePlugins: (Plugin | Plugin[])[] = [
+    // have to
+    vue(),
+    // have to
+    vueJsx(),
+  ];
+
+  !isBuild && vitePlugins.push(configHmrPlugin());
+
+  VITE_LEGACY && isBuild && vitePlugins.push(legacy());
+
+  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
+
+  vitePlugins.push(configSvgIconsPlugin(isBuild));
+
+  VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild));
+
+  vitePlugins.push(purgeIcons());
+
+  vitePlugins.push(configStyleImportPlugin(isBuild));
+
+  vitePlugins.push(configVisualizerConfig());
+
+  if (isBuild) {
+    //vite-plugin-imagemin
+    VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
+  }
+
+  return vitePlugins;
+}
